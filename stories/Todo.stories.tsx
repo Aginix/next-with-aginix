2187378df44a435ca7cc2bb7c24b93b72f22d406
n3n <nonpawit.tee@gmail.com>
2021-12-28 20:39:30 +0700

    Fix build failed

--- stories/Todo.stories.tsx
+++ stories/Todo.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
 
-import { TodoListItem } from '../components/todo';
+import { TodoListItem } from '../src/components/todo';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
